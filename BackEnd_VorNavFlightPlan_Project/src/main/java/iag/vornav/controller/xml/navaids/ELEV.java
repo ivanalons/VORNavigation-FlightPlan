1db22a0a3897e829a6be3ea51477ae1e2c3c4bbd
Ivan Alonso <ivanalons@gmail.com>
2020-09-25 11:22:52 +0200

    FASE 1 COMPLETED - PARSING XML TO JAVA OBJECT AT REQUESTBODY REST

--- BackEnd_VorNavFlightPlan_Project/src/main/java/iag/vornav/controller/xml/navaids/ELEV.java
+++ BackEnd_VorNavFlightPlan_Project/src/main/java/iag/vornav/controller/xml/navaids/ELEV.java
@@ -1,12 +1,13 @@
 package iag.vornav.controller.xml.navaids;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlText;
 
 public class ELEV {
 
 	@JsonProperty("UNIT")
 	private String UNIT;
 	
-	@JsonProperty("")
-	private int elev;
+	@JacksonXmlText
+	private int value;
 }
